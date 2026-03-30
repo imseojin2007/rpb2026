fb29afae7f823e1369e36992544b037d380b9fe9
imseojin2007 <imseojin2007@snu.ac.kr>
2026-03-30 18:01:04 +0900

    update calc.py
    add, divide

--- calc.py
+++ calc.py
@@ -1,1 +1,29 @@
 def main():
+    print("Let's implement addition. Type two numbers for x and y.")
+
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d + %d = %d" % (x, y, add(x, y)))
+    
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+    
+    
+def add(x, y):
+    return x + y
+    
+    
+def divide(x, y):
+    if y == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        return x/y   
+
+
+if __name__ == "__main__":
+    main()
